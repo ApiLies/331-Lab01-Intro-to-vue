1ccefa46259bbcf91901e0f5b021fe711a3b0fb4
ApiLies <Apisit_Liesmann@cmu.ac.th>
2025-07-03 17:04:14 +0700

    Checkpoint 8.5

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const { createApp, ref } = Vue
+const { createApp, ref, computed } = Vue
 
 createApp({
     setup(){
         const product = ref('Boots')
-        const image = ref('./assets/images/socks_green.jpg')
-        const inStock = ref(false)
+        const brand = ref('SE 331')
+       // const image = ref('./assets/images/socks_green.jpg')
+       // const inStock = ref(false)
         const inventory = ref(100)
         const details = ref([
             '50% cotton',
@@ -12,21 +13,40 @@ createApp({
             '20% polyyester'
         ])
         const variants = ref([
-            {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-            {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+            {id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity: 50},
+            {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 5}
         ])
+        const selectedVariant = ref(0)
+
+        const onSale = ref(true)
+        const saleMessage = computed(() => {
+            return onSale.value ? `${brand.value} ${product.value} is on sale` : ''
+        })
 
         const cart = ref(0)
 
         function addToCart(){
             cart.value += 1
         }
+        const title = computed(() =>{
+            return brand.value + ' ' + product.value
+        })
         function updateImage(variantImage){
             image.value = variantImage
         }
+
+        function updateVariant(index){
+            selectedVariant.value = index;
+        }
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image
+        })
+        const inStock = computed(() => {
+            return variants.value[selectedVariant.value].quantity
+        })
         
         return {
-            product,
+            title,
             image,
             inStock,
             inventory,
@@ -34,10 +54,13 @@ createApp({
             variants,
             cart,
             addToCart,
-            updateImage
+            updateImage,
+            updateVariant,
+            onSale,
+            saleMessage
         }
     }
 
 }).mount('#app')
 
-//checkpoint 7.6
+//checkpoint 8.5
